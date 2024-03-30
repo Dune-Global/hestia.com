b525e6fc0a1bf9b5193bf3fd144459b4f0901904
Lithara Perera <litharaperera2002@gmail.com>
2024-03-30 22:31:49 +0530

    refactor: Folder structure

---
+++ src/app/(client)/admin/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function Admin() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Admin
+    </main>
+  );
+}

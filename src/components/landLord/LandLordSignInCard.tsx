e6080fca7ad6779f7a97c1317f0df5d63151bcaa
yasithranusha <yasithranusha24@gmail.com>
2024-03-31 00:45:25 +0530

    feat: Refactor folder structure

---
+++ src/components/landLord/LandLordSignInCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const LandLordSignInCard = (props: Props) => {
+  return (
+    <div>LandLordSignInCard</div>
+  )
+}
+
+export default LandLordSignInCard

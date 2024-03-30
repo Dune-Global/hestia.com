aef47ff7d8c248a7023329f1e322305a775aec2a
yasithranusha <yasithranusha24@gmail.com>
2024-03-30 21:46:00 +0530

    refactor: Folder Structure

---
+++ src/app/(server)/models/landLord.ts
@@ -0,0 +1,29 @@
+import mongoose,{models} from "mongoose";
+
+const landLordSchema = new mongoose.Schema({
+    fullName: {
+        type: String,
+        required: true,
+    },
+    userName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+

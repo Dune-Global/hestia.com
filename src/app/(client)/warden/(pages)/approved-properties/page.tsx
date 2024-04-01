b7a0262bd3ca0394ee51dddd3c8d8ef779c8741b
Lithara Perera <litharaperera2002@gmail.com>
2024-04-02 00:30:13 +0530

    feat: Warden removed properties and approved properties

--- src/app/(client)/warden/(pages)/approved-properties/page.tsx
+++ src/app/(client)/warden/(pages)/approved-properties/page.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Properties } from "@/data/properties";
-import PendingPropertyCard from "@/components/common/cards/PropertyCard";
+import PropertyCard from "@/components/common/cards/PropertyCard";
 import PageHeader from "@/components/common/layout/PageHeader";
 import Container from "@/components/common/Container";
+import { Button } from "@/components/ui/button";
 
-export default function PendingPropertiesPage() {
+export default function ApprovedPropertiesPage() {
   return (
     <Container>
-      <PageHeader title="Pending Properties" />
+      <PageHeader title="Approved Properties" />
       <div className="pb-16 grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 gap-8">
         {Properties.map((property) => (
-          <div key={property.id}>
-            <PendingPropertyCard
+          <div key={property.id} className="border border-hgray-400 rounded-lg">
+            <PropertyCard
               id={property.id}
               image={property.image}
               name={property.name}
@@ -20,6 +21,13 @@ export default function PendingPropertiesPage() {
               beds={property.beds}
               bathrooms={property.bathrooms}
             />
+            <div className="flex gap-3 mx-4 mb-4">
+              <div>
+                <Button variant="outline" size="sm">
+                  Remove
+                </Button>
+              </div>
+            </div>
           </div>
         ))}
       </div>

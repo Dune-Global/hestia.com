9d883b8ed016d9b483ae86f079b1a6ff38621d0b
SanudaKJ <sanudajayasinghe18@gmail.com>
2024-04-07 05:07:48 +0530

    refactor: Nav and Footer

--- src/app/(client)/admin/(pages)/preview-article/page.tsx
+++ src/app/(client)/admin/(pages)/preview-article/page.tsx
@@ -23,7 +23,7 @@ export default function PreviewArticlesPage() {
           <div>
             In the fast-paced world of real estate rentals, finding the ideal
             accommodation can be a daunting task. From navigating listings to
-            understanding rental agreements, there's a lot to consider.
+            understanding rental agreements,  there &apos s a lot to consider.
             Fortunately, with the right guidance, you can streamline the process
             and secure the perfect place to call home.
           </div>
@@ -31,7 +31,7 @@ export default function PreviewArticlesPage() {
             1. Start with a Clear Vision: Before diving into the rental market,
             take some time to define your needs and preferences. Consider
             factors such as location, budget, size, amenities, and proximity to
-            essential services. Having a clear vision of what you're looking for
+            essential services. Having a clear vision of what you &apos re looking for
             will help you narrow down your options and focus your search.
           </div>
           <div>
@@ -42,11 +42,11 @@ export default function PreviewArticlesPage() {
             criteria to save time and find listings that match your preferences.
           </div>
           <div>
-            3. Attend Open Houses and Inspections: Once you've identified
+            3. Attend Open Houses and Inspections: Once you&aposve identified
             potential properties, attend open houses or schedule inspections to
             see them in person. This will give you a better sense of the
-            property's condition, layout, and surroundings. Take note of any red
-            flags or deal-breakers and don't hesitate to ask questions about the
+            property&aposs condition, layout, and surroundings. Take note of any red
+            flags or deal-breakers and don&apost hesitate to ask questions about the
             property or rental terms.
           </div>
           <div>
@@ -54,14 +54,14 @@ export default function PreviewArticlesPage() {
             make sure you thoroughly understand the terms of the rental
             agreement. Pay close attention to clauses related to rent,
             utilities, security deposits, maintenance responsibilities, and
-            lease duration. If anything seems unclear or unreasonable, don't
+            lease duration. If anything seems unclear or unreasonable, don&apost
             hesitate to seek clarification or negotiate terms with the landlord
             or property manager.
           </div>
           <div>
             5. Consider Additional Factors: In addition to the property itself,
             consider other factors that may impact your living experience.
-            Research the neighborhood's safety, accessibility to public
+            Research the neighborhood&aposs safety, accessibility to public
             transportation, proximity to schools or workplaces, and availability
             of amenities such as grocery stores, parks, and recreational
             facilities. Visiting the neighborhood at different times of the day
@@ -69,8 +69,8 @@ export default function PreviewArticlesPage() {
             lifestyle.
           </div>
           <div>
-            6. Seek Professional Advice: If you're new to the rental market or
-            feeling overwhelmed, don't hesitate to seek advice from
+            6. Seek Professional Advice: If you&aposre new to the rental market or
+            feeling overwhelmed, don&apost hesitate to seek advice from
             professionals such as real estate agents or property managers. They
             can provide valuable insights, help you navigate the rental process,
             and advocate on your behalf during negotiations.

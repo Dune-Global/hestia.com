b26c92e8aa2bea748bab8584ce5b675248ba1eb9
yasithranusha <yasithranusha24@gmail.com>
2024-04-07 04:49:57 +0530

    feat: Add Property

---
+++ src/app/(server)/models/property.ts
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+import { Gender } from "@/enum/Gender";
+import { PropertyType, ShareType } from "@/enum/Property";
+import LandLordModel from "./landLord";
+
+const googleMapLocationSchema = new mongoose.Schema({
+  latitude: Number,
+  longitude: Number,
+});
+
+const addressSchema = new mongoose.Schema({
+  line1: String,
+  line2: String,
+  line3: String,
+  line4: String,
+  city: String,
+  stateOrProvince: String,
+  postalCode: String,
+  googleMapLocation: googleMapLocationSchema,
+});
+
+const amenitiesSchema = new mongoose.Schema({
+  wifi: Boolean,
+  tv: Boolean,
+  airConditioning: Boolean,
+  pool: Boolean,
+  outdoorDining: Boolean,
+  exercise: Boolean,
+  firstAid: Boolean,
+  fireExtinguisher: Boolean,
+  smokeAlarm: Boolean,
+});
+
+const basicsSchema = new mongoose.Schema({
+  guests: Number,
+  bedrooms: Number,
+  bedsPerRoom: Number,
+  bathrooms: Number,
+});
+
+const propertySchema = new mongoose.Schema({
+  landlord: { type: mongoose.Schema.Types.ObjectId, ref: LandLordModel },
+  name: String,
+  description: String,
+  address: addressSchema,
+  propertyType: {
+    type: String,
+    enum: PropertyType,
+  },
+  shareType: {
+    type: String,
+    enum: ShareType,
+  },
+  amenities: amenitiesSchema,
+  images: [String],
+  genders: {
+    type: String,
+    enum: Gender,
+  },
+  basics: basicsSchema,
+});
+
+const Property = mongoose.model("Property", propertySchema);
+
+export default Property;

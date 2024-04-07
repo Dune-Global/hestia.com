a1d42ae7798dae8cddb606865424cb72820f280e
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-04-08 02:36:35 +0530

    refactor: Feature card

--- src/components/common/cards/LongCard.tsx
+++ src/components/common/cards/LongCard.tsx
@@ -1,18 +1,29 @@
-"use client";
-
 import React from "react";
 
 interface LongCardProps {
   svg: React.ReactElement;
   text: string;
   bottomText: string;
+  selected: boolean;
+  onClick: () => void;
 }
 
-const LongCard: React.FC<LongCardProps> = ({ svg, text, bottomText }) => {
+const LongCard: React.FC<LongCardProps> = ({
+  svg,
+  text,
+  bottomText,
+  selected,
+  onClick,
+}) => {
   return (
-    <div className="max-w-[740px] min-h-[115px] bg-white border-2 border-hgray-300 rounded-xl py-4 px-6 flex justify-between items-center cursor-pointer">
+    <div
+      className={`max-w-[740px] min-h-[115px] bg-white border-2 border-hgray-300 rounded-xl py-4 px-6 flex justify-between items-center cursor-pointer ${
+        selected ? "bg-gray-200" : ""
+      }`}
+      onClick={onClick}
+    >
       <div className="flex flex-col h-full gap-2">
-        <span className="font-medium text-[18px] leading-[24px] ">{text}</span>
+        <span className="font-medium text-[18px] leading-[24px]">{text}</span>
         <span className="text-hgray-500 text-[16px] leading-[24px]">
           {bottomText}
         </span>
